b028d3c9651a6ced8fb1904c44f2dec3cff93519
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-11 14:32:39 +0200

    Update Project Euler #34 justification.py

--- Project Euler #34 justification.py
+++ Project Euler #34 justification.py
@@ -1,6 +1,7 @@
 #since 10^n grows much faster than n*9!
 #d(n*9!)/dn is a constant (9!) while d(10^n)/dn = (10^n)*ln(10)
 #there are clearly no sinlge or double digit factorials apart from 1 and 2 since since 3! neq 6  and 4! > 9.
+#5! > 99. So if there is a double digit factorial, then its bounded from below by 10 and above by 44.
 import time
 import math
 
