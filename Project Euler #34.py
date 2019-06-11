4fffb3c6297f48fc9acd25e58198bf8f0b86a109
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-11 11:35:09 +0200

    Update Project Euler #34.py

--- Project Euler #34.py
+++ Project Euler #34.py
@@ -10,11 +10,26 @@ def factorial_generator(lower_bound,upper_bound):
 
 factorial_generator(1,9)
 
-def Digit_factorials_3digits(lower_bound,upper_bound,x,a,b,c):
-    while x > upper_bound:
-        x = Factorials[a] + Factorials[b] + Factorials[c]
+def Digit_factorials_3digits(x,a,b,c):
+    while c < 5:
+        if x == Factorials[a] + Factorials[b] + Factorials[c]:
+            Digit_Factorials.append(x)
+        if a != 5:
+            a += 1
+        else:
+            b += 1
+            a = 0
+        if b == 5:
+            c += 1
+            b = 0
+        if c == 5:
+            x += 1
+            c = 0
+            if x == 999:
+                break
 
-Digit_factorials(144,1000,100,0,0,0)
-print(sum(Digit_Factorials))
+
+Digit_factorials_3digits(100,0,0,0)
+print((Digit_Factorials))
 end = time.time()
 print(end - start)
